c3a8a72db8f129148c0b95116b487c48b87762b8
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2024-04-23 20:56:14 -0400

    enable portfolio for diana

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/')]
 final class HomeController extends AbstractController
 {
 
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        return $this->render('default/homepage.html.twig');
+        $portfolioUrls = [];
+        $portfolioNames = $this->getParameter('portfolios');
+        sort($portfolioNames);
+        foreach ($portfolioNames as $portfolioName) {
+            $portfolioUrls[$portfolioName] = $this->generateUrl('portfolio', [
+                'subdomain' => $portfolioName,
+                'domain' => $this->getParameter('ext')
+            ], UrlGeneratorInterface::ABSOLUTE_URL);
+        }
+
+        return $this->render('default/homepage.html.twig', [
+            'portfolioUrls' => $portfolioUrls,
+        ]);
     }
 }
