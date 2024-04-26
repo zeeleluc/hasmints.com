8606a8d6e545a7db02c35e7c3716afccad554bfe
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2024-04-25 20:34:04 -0400

    enable bootstrap responsible popper menu

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,11 @@ import './styles/app.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/css/v4-shims.css';
 import 'lato-font/css/lato-font.css';
+
+// loads the Bootstrap plugins
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/modal';
+import 'jquery'
